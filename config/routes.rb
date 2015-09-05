170775dfdf6da31a953f377f91cf8ffee501219d
Kyle Annen <kyleannen@gmail.com>
2015-09-04 21:06:49 -0700

    finish login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get "/about" => "static_pages#about"
+
+  get "/help" => "satic_pages#help"
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
